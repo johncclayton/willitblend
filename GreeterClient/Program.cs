5642fd74c50f9e68eac34455e0a9f031183f1d86
John Clayton <johnc@filewave.com>
2020-11-27 23:07:52 +0100

    Fix cert thing

--- GreeterClient/Program.cs
+++ GreeterClient/Program.cs
@@ -9,11 +9,19 @@ namespace GreeterClient
     {
         static async Task Main(string[] args)
         {
-            // DO THIS FIRST: https://docs.microsoft.com/en-us/aspnet/core/security/enforcing-ssl?view=aspnetcore-5.0#trust-the-aspnet-core-https-development-certificate-on-windows-and-macos
+            // reference when certs come knocking on your validated door
+            // https://docs.microsoft.com/en-us/aspnet/core/security/enforcing-ssl?view=aspnetcore-5.0#trust-the-aspnet-core-https-development-certificate-on-windows-and-macos
 
-            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            // OR; just ignore the darn cert validation error
+            var httpHandler = new HttpClientHandler();
+            // Return `true` to allow certificates that are untrusted/invalid
+            httpHandler.ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001", 
+                new GrpcChannelOptions { HttpHandler = httpHandler } );
             var client = new Greeter.GreeterClient(channel);
-            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Stupid" });
+            var reply = await client.BlendItForMeAsync(new HelloRequest { Name = "Stupid" });
             Console.WriteLine("Greeting: " + reply.Message);
             Console.WriteLine("Press any stupid key to exit...");
             Console.ReadKey();
